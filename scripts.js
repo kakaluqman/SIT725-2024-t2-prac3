0be40c7b956bc8cb6cc07aa0da89130ef7c0657b
kakaluqman <163084583+kakaluqman@users.noreply.github.com>
2024-04-02 23:46:05 +1100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const clickMe = () => {
+
+    alert("Thanks for clicking me. Hope you have a nice day!")
+    
+    }
+    
+    $(document).ready(function() {
+    
+    $('.materialboxed').materialbox();
+    
+    $('.clickMeButton').click(()=>{
+    
+    clickMe();
+    
+    })
+    
+    });
